c49f9580c887d5b66f00ec7945580f5650560e48
erensapci <43892645+erensapci@users.noreply.github.com>
2020-10-29 22:19:03 +0300

    All file edit

---
+++ DataAccess/Concrete/Entityframework/EfProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess.Concrete.EntityFramework;
+using DataAccess. Abstract;
+using DataAccess.Concrete.Entityframework.Contexts;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.Entityframework
+{
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>, IProductDal
+    {
+      
+    }
+}
